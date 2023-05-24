8d12975aecc34de4d4ec4fbc4170240b58e649d5
Tri <trimpham98@gmail.com>
2023-05-24 02:25:13 -0700

    Add axes helper and move camera to see all 3 axes

--- src/script.js
+++ src/script.js
@@ -20,6 +20,10 @@ mesh.position.set(0.7, -0.6, 1);
 
 scene.add(mesh);
 
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 /**
  * Sizes
  */
@@ -33,6 +37,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 1;
 scene.add(camera);
 
 /**

c67507cdc9f6e2f158062f01435d897b6b204ea3
Tri <trimpham98@gmail.com>
2023-05-24 23:25:27 -0700

    Add mesh rotation reorder before rotation

--- src/script.js
+++ src/script.js
@@ -12,13 +12,23 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
 // mesh.position.x = 0.7;
 // mesh.position.y = - 0.6;
 // mesh.position.z = 1;
-
 mesh.position.set(0.7, -0.6, 1);
 
-scene.add(mesh);
+// Scale 
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder('YXZ');
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -36,7 +46,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 4;
 camera.position.y = 1;
 camera.position.x = 1;
 scene.add(camera);

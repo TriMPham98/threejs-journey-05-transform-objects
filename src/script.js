a35f73eb80776b5aeb457929e845108f5df464c5
Tri <trimpham98@gmail.com>
2023-05-24 23:48:08 -0700

    Add group of three rectangles and transform group

--- src/script.js
+++ src/script.js
@@ -9,26 +9,35 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
 
-// mesh.position.x = 0.7;
-// mesh.position.y = - 0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group. rotation.y = 1;
+scene.add(group);
 
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1,),
+  new THREE.MeshBasicMaterial({color: 'red'})
+);
 
-// Rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1,),
+  new THREE.MeshBasicMaterial({color: 'green'})
+);
+
+cube2.position.x = -1.5;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1,),
+  new THREE.MeshBasicMaterial({color: 'blue'})
+);
+
+cube3.position.x = 1.5;
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -52,7 +61,6 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.set(0, 0, 3);
 scene.add(camera);
 
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
